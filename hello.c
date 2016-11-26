bd4d9721c1558ea5fb05e1a318dd97fca5d5bdbd
Calson Noah <chiacalson@gmail.com>
2016-11-26 17:26:17 +0100

    Added another line the the program

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
 #include <include>//My first project git project
 int main()
 {
-	printf("Hello World!!!");
+	printf("Hello World!!!\n");
+	printf("It is cool learning to use git and github\n");
 	return 0;
 }
